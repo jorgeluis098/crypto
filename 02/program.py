45ebad3c9bad1a50bc9f8997cc1dc440382c8979
jorgeluis098 <jorge.luis.rb.98@gmail.com>
2021-06-08 18:31:00 -0500

    finalizado rc4

--- 02/program.py
+++ 02/program.py
@@ -1,11 +1,9 @@
 import fileinput
 import math
 from operator import xor # para poder realizar la operacion xor
-
-# lines = []
-# for line in fileinput.input():
-#     lines.append(line)
-
+lines = []
+for line in fileinput.input():
+    lines.append(line.strip())
 #algoritmo KSA
 S=[]
 T=[]
@@ -13,9 +11,8 @@ keyStream=[]
 mensaje=""
 n=0
 j=0
-key="Wiki"
-#Se llena el vector S con numeros  0-255 y T con la key
-for i in range(256):
+key=lines[0]
+for i in range(256):#Se llena el vector S con numeros  0-255 y T con la key
     if n== len(key):
         n=0
     S.append(i)
@@ -27,14 +24,16 @@ for i in range(256):
     temp = S[i]
     S[i] = S[j] 
     S[j] = temp
-#PRGA
-j=0
-entrada="Plaintext"
+j=0  #PRGA
+entrada=lines[1]
 for i in range(1,len(entrada)+1):
     j=((j + S[i]) % 256)
     temp = S[i]
     S[i] = S[j] 
     S[j] = temp
     keyStream.append(S[(S[i]+S[j]) % 256])
-    mensaje= mensaje + hex(xor(keyStream[i-1],ord(entrada[i-1]))).upper().split('X')[-1] # xor y se convierte a hex
+    if ( len(hex(xor(keyStream[i-1],ord(entrada[i-1]))).upper().split('X')[-1]) == 1):
+         mensaje= mensaje + "0" + hex(xor(keyStream[i-1],ord(entrada[i-1]))).upper().split('X')[-1]
+    else: 
+        mensaje= mensaje + hex(xor(keyStream[i-1],ord(entrada[i-1]))).upper().split('X')[-1] # xor y se convierte a hex
 print (mensaje)
